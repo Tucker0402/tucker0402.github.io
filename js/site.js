3fbeaf87744cecf5b8288de3b8bfd235254d5d1c
Ethan Rabideau <ethantuck212@gmail.com>
2025-09-28 16:58:49 -0500

    Added welcome div

--- js/site.js
+++ js/site.js
@@ -1,1 +1,20 @@
 //alert("bla")
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+if (isMorning)
+{
+    document.querySelector('div#welcome > h3').textContent = 'Good Morning,'
+}
+else if (isAfternoon)
+{
+    document.querySelector('div#welcome > h3').textContent = 'Good Afternoon,'
+}
+else
+{
+    document.querySelector('div#welcome > h3').textContent = 'Good Evening,'
+}
