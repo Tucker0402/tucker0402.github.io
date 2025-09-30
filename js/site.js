ef40a1b45807cc62f164cac06c748ad184270feb
Ethan Rabideau <ethantuck212@gmail.com>
2025-09-30 13:04:05 -0500

    Added secret message

--- js/site.js
+++ js/site.js
@@ -6,6 +6,8 @@ const isMorning = hours >= 4 && hours < 12 // is it morning?
 const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
 const isEvening = hours >= 17 || hours < 4 // is it evening?
 
+const secret = "Secret Message!"
+
 if (isMorning)
 {
     document.querySelector('div#welcome > h3').textContent = 'Good Morning,'
@@ -18,3 +20,5 @@ else
 {
     document.querySelector('div#welcome > h3').textContent = 'Good Evening,'
 }
+
+localStorage.setItem("It's a secret to everybody.", "I think its a reference to Zelda, but im not sure... Also saw your last message, ill get around to that exp farm eventually... maybe...")
